f0c1df67790583a6ed003323f743ff01bf83db4e
Lexo783 <florentdebuchy@orange.fr>
2021-11-29 13:46:43 +0100

    add methods

--- Exo1/Florent/index.js
+++ Exo1/Florent/index.js
@@ -1,29 +1,40 @@
 const crypto = require('crypto')
 
 const arraySymbol = ["♠", "♥", "♦", "♣"]
-let arrayResult = []
 
-for (let i = 0; i < 100; ++i) {
-    let tempo = []
-    for (let j = 0; j < 8 ; j++) {
-        tempo[j] = arraySymbol[crypto.randomInt(0, 4)]
+function GenerateSymbol(nbr, row) {
+    let arrayResult = []
+
+    for (let i = 0; i < nbr; ++i) {
+        let tempo = []
+        for (let j = 0; j < row ; j++) {
+            tempo[j] = arraySymbol[crypto.randomInt(0, 4)]
+        }
+        arrayResult[i] = tempo
     }
-    arrayResult[i] = tempo
+    return arrayResult
 }
-arrayResult.forEach(function (element, key) {
-    let bool = true
-    for (let i = 0; i < element.length; ++i) {
-        let stringos = ''
-        if(i !== i-1 || i !== 0){
-            if(element[i-1] === element[i] && element[i+1] === element[i] && bool){
-                stringos = 'index ' + key + ': ' + element[i-1] + ' ' + element[i] + ' ' + element[i+1]
-                bool = false
+
+function verif(arrayResult) {
+    arrayResult.forEach(function (element, key) {
+        let bool = true
+        for (let i = 0; i < element.length; ++i) {
+            let stringos = ''
+            if(i !== i-1 || i !== 0){
+                if(element[i-1] === element[i] && element[i+1] === element[i] && bool){
+                    stringos = 'index ' + key + ': ' + element[i-1] + ' ' + element[i] + ' ' + element[i+1]
+                    bool = false
+                }
+
+            }
+            if(stringos !== ''){
+                console.log(stringos)
             }
 
         }
-        if(stringos !== ''){
-            console.log(stringos)
-        }
+    })
+}
 
-    }
-})
+const array = GenerateSymbol(100, 8)
+
+verif(array)
