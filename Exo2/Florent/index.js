d19605212adce7e5bd6fba95d5f6af5f113fa8d3
Lexo783 <florentdebuchy@orange.fr>
2021-11-30 09:20:35 +0100

    add learnode

--- Exo2/Florent/index.js
+++ Exo2/Florent/index.js
@@ -22,16 +22,58 @@ function readeFile(path, option) {
 }
 
 // fonction async
-async function takeFile(){
-    try {
-        const data = await readeFile(processARGV[2], {encoding : 'utf-8'})
-        console.log(data)
-    }catch (err){
-        console.log(err)
-    }finally {
-        console.log('finale')
+async function putFile(routeName){
+    if (isGoodFormat(routeName)){
+        try {
+            return await writeFile('text.txt', {encoding : 'utf-8'})
+        }catch (err){
+            console.log(err)
+        }
     }
 }
 
-console.log()
-takeFile()
+function writeFile(path, option) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, option, (err, data) => {
+            if (err){
+                return reject(err)
+            }
+            resolve(data)
+        })
+    })
+}
+
+// fonction async
+async function takeFile(routeName){
+    if (isGoodFormat(routeName)){
+        try {
+            return await readeFile(routeName, {encoding : 'utf-8'})
+        }catch (err){
+            console.log(err)
+        }
+    }
+}
+
+function convertFile(csv){
+
+    let arr = csv.split('\n');
+    var jsonObj = [];
+    var headers = arr[0].split(';');
+    for(var i = 1; i < arr.length; i++) {
+        var data = arr[i].split(';');
+        var obj = [];
+        for(var j = 0; j < data.length; j++) {
+            obj[headers[j].trim()] = data[j].trim();
+        }
+        jsonObj.push(obj);
+    }
+
+    return jsonObj
+}
+
+async function main(){
+    const data = await takeFile(processARGV[2])
+    const c = convertFile(data)
+
+}
+main()
